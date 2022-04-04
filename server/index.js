4c456f06d16f6731ac3c5bca72277bf85083012d
Chamod Nugekotuwa <chamodnugekotuwa@gmail.com>
2022-04-04 22:51:41 +0530

    server updated

--- server/index.js
+++ server/index.js
@@ -17,9 +17,8 @@ const notificationService = require("./services/notificationService");
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("/", function (req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
 });
 
 app.use("/api/user", user);
