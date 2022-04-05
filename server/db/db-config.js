1950e2a3068bb75cbebc57d167d795079b70ebd6
Chamod Nugekotuwa <chamodnugekotuwa@gmail.com>
2022-04-05 07:03:40 +0530

    db changes

--- server/db/db-config.js
+++ server/db/db-config.js
@@ -4,20 +4,20 @@ const fabricate = require("./fabricate");
 
 // const DATABASE = "stuforum";
 
-// const db = mysql.createConnection({
-//   user: "bd53d86651de01",
-//   password: "a02da056",
-//   host: "eu-cdbr-west-02.cleardb.net",
-//   database: "heroku_0c59dc1ccefc3d5",
-// });
-
 const db = mysql.createConnection({
-  user: "root",
-  password: "",
-  host: "localhost",
+  password: "djyee6jxhu38olec",
+  host: "i54jns50s3z6gbjt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
   port: 3306,
-  // database: "stuforum",
+  database: "ek6jorwy8tgebmrn",
 });
+
+// const db = mysql.createConnection({
+//   user: "root",
+//   password: "",
+//   host: "localhost",
+//   port: 3306,
+//   // database: "stuforum",
+// });
 
 db.connect(function (err) {
   if (!err) {
